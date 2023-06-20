9bd843341dd0b893ecda99ce99df5c579e495bdc
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-06-20 10:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	var firstName=document.querySelector('input[name="fname"]').value;
+	var lastName=document.querySelector('input[name="lname"]').value;
+	alert(firstName+" "+lastName);
+	
 }
